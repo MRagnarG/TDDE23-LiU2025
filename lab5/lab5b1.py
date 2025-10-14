0646be76d3242d8ce2628626fe6b22a3d5ec8231
Matheus Ragnar Grönstedt <matheusragnar@gmail.com>
2025-10-14 13:26:48 +0200

    Ändrat filerna lab5 b1, b2 och b3 för att anpassa programmeringspraxis och PEP-8, samt skrev docstring till alla functioner.

--- lab5/lab5b1.py
+++ lab5/lab5b1.py
@@ -3,24 +3,48 @@ from lab5a import cvimg_to_list
 from cvlib import greyscale_list_to_cvimg
 
 
-def pixel_constraint(hlow, hhigh, slow, shigh, vlow, vhigh):
+def pixel_constraint(hlow:int, hhigh:int, slow:int, shigh:int, vlow:int, vhigh:int):
+    """Function that creates a new function that, given a pixel, returns if    
+    it is between desired interval.
 
-    def is_in_interval(pixel):
-        (h,s,v) = pixel
-        if hlow<= h <= hhigh and slow<= s <= shigh and vlow<= v <= vhigh:
+    Args:
+        hlow (int): lowest value for hue
+        hhigh (int): highest value for hue
+        slow (int): lowest value for saturation
+        shigh (int): highest value for saturation
+        vlow (int): lowest value for value
+        vhigh (int): highest value for value
+    """
+    def is_in_interval(pixel:tuple):
+        """Function that checks if a given pixel is in a pre decided interval.
+
+        Args:
+            pixel (tuple): hsv_pixel
+
+        Returns:
+            1 if the pixel is in the desired interval, 
+            0 if it's not.
+        """
+        (h, s, v) = pixel
+        if hlow <= h <= hhigh and slow <= s <= shigh and vlow <= v <= vhigh:
             return 1
         else:
             return 0
-    
+
     return is_in_interval
+
 
 if __name__ == "__main__":
     hsv_plane = cv2.cvtColor(cv2.imread("plane.jpg"), cv2.COLOR_BGR2HSV)
     plane_list = cvimg_to_list(hsv_plane)
-    
+
     is_sky = pixel_constraint(100, 150, 50, 200, 100, 255)
     sky_pixels = list(map(lambda x: x * 255, map(is_sky, plane_list)))
-    
-    cv2.imshow('sky', greyscale_list_to_cvimg(sky_pixels, hsv_plane.shape[0], hsv_plane.shape[1]))
-    cv2.waitKey(0)
 
+    cv2.imshow(
+        "sky",
+        greyscale_list_to_cvimg(
+            sky_pixels, hsv_plane.shape[0], hsv_plane.shape[1]
+        ),
+    )
+    cv2.waitKey(0)
--- lab5/lab5b2.py
+++ lab5/lab5b2.py
@@ -3,28 +3,45 @@ from lab5a import cvimg_to_list
 from cvlib import rgblist_to_cvimg
 
 
-def generator_from_image (img):
+def generator_from_image(img: cv2.AsyncArray):
+    """Function that given an array with pixels in it, will use a intern 
+    function to return a specified pixel color in hsv format.
 
-    def pixel_color(i):
+    Args:
+        img (cv2.AsyncArray): image pixels organized as an array.
+    """
 
-        if not img[i]:
+    def pixel_color(index: int):
+        """Function that given a previous array and now a index, returns
+        the color or that pixel.
+
+        Args:
+            index (int): index from the pixels on the array
+
+        Returns:
+            int or tuple: if there's no pixel on that index, returns 0,
+            otherwise, returns pixels color.
+        """
+        if not img[index]:
             return 0
-        
+
         else:
-            return img[i]
-        
+            return img[index]
+
     return pixel_color
 
 
 if __name__ == "__main__":
     orig_img = cv2.imread("plane.jpg")
-    
+
     orig_list = cvimg_to_list(orig_img)
-    
+
     generator = generator_from_image(orig_list)
-    
+
     new_list = [generator(i) for i in range(len(orig_list))]
-    
-    cv2.imshow('original', orig_img)
-    cv2.imshow('new', rgblist_to_cvimg(new_list, orig_img.shape[0], orig_img.shape[1]))
+
+    cv2.imshow("original", orig_img)
+    cv2.imshow(
+        "new", rgblist_to_cvimg(new_list, orig_img.shape[0], orig_img.shape[1])
+    )
     cv2.waitKey(0)
